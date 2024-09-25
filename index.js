7cdd21cfc4c28ecb710660084f9449220ee4627e
totoargumedo <totoargumedo@gmail.com>
2024-09-25 13:59:17 -0300

    CRUD de front terminado

--- index.js
+++ index.js
@@ -1,12 +1,95 @@
-import http from "http";
+import express, { urlencoded } from "express";
 import "dotenv/config";
+import mongoose, { Schema, model } from "mongoose";
 
-function requestController() {
-  console.log("Holis");
-}
-
-const server = http.createServer(requestController);
-
+const app = express();
 const PORT = process.env.PORT || 8080;
 
-server.listen(PORT);
+//DB
+mongoose
+  .connect(process.env.MONGO_DB_URI)
+  .then(() => {
+    console.log("🟢 DB conectada correctamente");
+  })
+  .catch((error) => {
+    console.log("🔴 Error en la DB: " + error);
+  });
+const taskSchema = new Schema({ name: String, done: Boolean });
+const taskModel = model("tasks", taskSchema);
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+// app.use((req, res, next) => {});
+
+app.get("/api/tasks", (req, res) => {
+  taskModel
+    .find({})
+    .then((response) => {
+      res.status(200).json({
+        ok: true,
+        message: "Tarea obtenidas con exito",
+        data: response,
+      });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        ok: false,
+        message: "Error al obtener las tareas",
+      });
+    });
+});
+
+app.post("/api/tasks", (req, res) => {
+  const data = req.body;
+  taskModel
+    .create({ name: data.text, done: false })
+    .then((response) => {
+      res
+        .status(201)
+        .json({ ok: true, message: "Tarea creada con exito", data: response });
+    })
+    .catch((error) => {
+      res.status(400).json({ ok: false, message: "Error al crear la tarea" });
+    });
+});
+app.put("/api/tasks/:id", (req, res) => {
+  const data = req.body;
+  const { id } = req.params;
+  taskModel
+    .findByIdAndUpdate(id, { name: data.text }, { new: true })
+    .then((response) => {
+      res.status(200).json({
+        ok: true,
+        message: "Tarea actualizada con exito",
+        data: response,
+      });
+    })
+    .catch((error) => {
+      res
+        .status(400)
+        .json({ ok: false, message: "Error al actualizar la tarea" });
+    });
+});
+
+app.delete("/api/tasks/:id", (req, res) => {
+  const { id } = req.params;
+  taskModel
+    .findByIdAndDelete(id)
+    .then((response) => {
+      res.status(200).json({
+        ok: true,
+        message: "Tarea eliminada con exito",
+        data: response,
+      });
+    })
+    .catch((error) => {
+      res
+        .status(400)
+        .json({ ok: false, message: "Error al eliminar la tarea" });
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`🟢 Server corriendo en puerto ${PORT}`);
+});
---
+++ public/js/index.js
@@ -0,0 +1,77 @@
+const getBtn = document.querySelector("#get-tasks");
+const createEditBtn = document.querySelector("#create-tasks");
+const input = document.querySelector("#task-name");
+const tasksContainer = document.querySelector("#tasks-container");
+let TASK_TO_EDIT = null;
+
+getBtn.addEventListener("click", async (e) => {
+  await getTasks();
+});
+
+createEditBtn.addEventListener("click", (e) => {
+  const creating = !TASK_TO_EDIT;
+  const path = creating ? "/api/tasks" : `/api/tasks/${TASK_TO_EDIT._id}`;
+  const method = creating ? "POST" : "PUT";
+  fetch(path, {
+    method: method,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ text: input.value }),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((responseJSON) => {
+      input.value = "";
+      input.focus();
+      createEditBtn.innerText = "Crear tarea";
+      TASK_TO_EDIT = null;
+      getTasks();
+    })
+    .catch((error) => console.log("Error post tareas"));
+});
+
+function getTasks() {
+  tasksContainer.innerHTML = "";
+  fetch("/api/tasks")
+    .then((response) => {
+      return response.json();
+    })
+    .then((responseJSON) => {
+      const tasks = responseJSON.data;
+      for (const task of tasks) {
+        const container = document.createElement("div");
+        const taskText = document.createElement("p");
+        taskText.innerText = task.name;
+        const taskDeleteBtn = document.createElement("button");
+        taskDeleteBtn.innerText = "Borrar";
+        taskDeleteBtn.setAttribute("id", task._id);
+        taskDeleteBtn.addEventListener("click", async (e) => {
+          await deleteTasks(e, taskDeleteBtn);
+        });
+        taskText.addEventListener("click", (e) => {
+          input.value = task.name;
+          createEditBtn.innerText = "Editar tarea";
+          TASK_TO_EDIT = task;
+        });
+        container.appendChild(taskText);
+        container.appendChild(taskDeleteBtn);
+        tasksContainer.appendChild(container);
+      }
+    })
+    .catch((error) => console.log("Error get tareas" + error));
+}
+function deleteTasks(event, button) {
+  const tid = event.target.id;
+  button.innerText = "...";
+  fetch(`/api/tasks/${tid}`, {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ text: input.value }),
+  })
+    .then((response) => {
+      button.parentElement.remove();
+    })
+    .catch((error) => console.log("Error post tareas"));
+}
+
+getTasks();
